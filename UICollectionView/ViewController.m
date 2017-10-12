036aeb9463801db51000571b2193e3cccb7b1efb
dym <dym@dymdemacbook-pro.local>
2017-10-12 15:42:41 +0800

    首页

--- UICollectionView/ViewController.m
+++ UICollectionView/ViewController.m
@@ -8,8 +8,9 @@
 
 #import "ViewController.h"
 
-@interface ViewController ()
-
+@interface ViewController ()<UICollectionViewDelegate,UICollectionViewDataSource>
+@property (nonatomic,strong) UICollectionView *collectionView;
+@property (nonatomic,strong) NSArray *dataSouce;
 @end
 
 @implementation ViewController
@@ -17,6 +18,45 @@
 - (void)viewDidLoad {
     [super viewDidLoad];
     // Do any additional setup after loading the view, typically from a nib.
+    self.title = @"UICollectionView";
+    [self.view addSubview:self.collectionView];
+    self.dataSouce = @[@"横向浏览相册",@"瀑布流",@"多个section多个样式",@"",@""];
+}
+
+- (NSInteger)collectionView:(UICollectionView *)collectionView numberOfItemsInSection:(NSInteger)section{
+    return self.dataSouce.count;
+}
+
+- (__kindof UICollectionViewCell*)collectionView:(UICollectionView *)collectionView cellForItemAtIndexPath:(NSIndexPath *)indexPath{
+    UICollectionViewCell *cell = [collectionView dequeueReusableCellWithReuseIdentifier:@"UICollectionViewCell" forIndexPath:indexPath];
+    NSString *str = self.dataSouce[indexPath.row];
+    UILabel *label = [[UILabel alloc]initWithFrame:cell.bounds];
+    label.text = str;
+    label.textAlignment = NSTextAlignmentCenter;
+    label.font = [UIFont systemFontOfSize:18];
+    [cell.contentView addSubview:label];
+    cell.backgroundColor = [UIColor redColor];
+    return cell;
+}
+
+- (void)collectionView:(UICollectionView *)collectionView didSelectItemAtIndexPath:(NSIndexPath *)indexPath{
+    
+}
+
+
+- (UICollectionView*)collectionView{
+    if (!_collectionView) {
+        UICollectionViewFlowLayout *flowLayout = [[UICollectionViewFlowLayout alloc]init];
+        flowLayout.itemSize = CGSizeMake(self.view.frame.size.width, 60);
+        flowLayout.minimumLineSpacing = 10;
+        
+        _collectionView = [[UICollectionView alloc]initWithFrame:self.view.bounds collectionViewLayout:flowLayout];
+        _collectionView.backgroundColor = [UIColor whiteColor];
+        _collectionView.delegate = self;
+        _collectionView.dataSource = self;
+        [_collectionView registerClass:[UICollectionViewCell class] forCellWithReuseIdentifier:@"UICollectionViewCell"];
+    }
+    return _collectionView;
 }
 
 
