bd8315a0ea1979cdf77cf1a19fadc863ab45ca75
dym <dym@dymdeMacBook-Pro.local>
2017-10-12 14:45:49 +0800

    first commit

---
+++ UICollectionView/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  UICollectionView
+//
+//  Created by dym on 2017/10/12.
+//  Copyright © 2017年 dym. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
